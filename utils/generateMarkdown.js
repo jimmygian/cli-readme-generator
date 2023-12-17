229cd0defe7637e1036ecc579532da6f246a36c5
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-12-17 23:29:48 +0000

    Corrects generateMarkdown variables

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.project_name}
+  return `# ${data.project_name.trim()}
 
-  ![License](https://img.shields.io/badge/license-${data.licence}-brightgreen)
+  ![License](https://img.shields.io/badge/license-${data.licence.trim()}-brightgreen)
   
   ## Description
   
-  ${data.project_desc}  
+  ${data.project_desc.trim()}  
   
 
   ## Table of Contents
@@ -24,36 +24,40 @@ function generateMarkdown(data) {
   To install the necessary dependencies, run the command: 
 
   \`\`\`
-  ${data.dep_command}
+  ${data.dep_command.trim()}
   \`\`\`
 
 
   ## Usage
 
-  ${data.usage}
+  ${data.usage.trim()}
   
 
   ## Tests
   
-  To run tests, use the command: \`${data.test_command}\`
+  To run tests, use the command: 
+  
+  \`\`\`
+  ${data.test_command.trim()}
+  \`\`\`
 
 
   ## Contributing
 
-  ${data.contributions}
+  ${data.contributions.trim()}
   
 
   ## License
   
-  This project is licensed under the [${data.licence}](LICENSE) license.
+  This project is licensed under the [${data.licence.trim()}](LICENSE) license.
   
 
   ## Questions
   
   If you have any questions or concerns, please feel free to reach out to me:
   
-  - GitHub: [${data.github_username}](https://github.com/${data.github_username})
-  - Email: ${data.email}
+  - GitHub: [${data.github_username.trim()}](https://github.com/${data.github_username.trim()})
+  - Email: ${data.email.trim()}
 
   `;
 }
